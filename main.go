5fefea7be183f6206d9353e1e0435a644c3e8dee
moris <moris.zhang@gmail.com>
2017-04-01 12:52:09 +0800

    can work

--- main.go
+++ main.go
@@ -6,12 +6,52 @@ import (
 	"log"
 	"net/http"
 
+	"time"
 	"github.com/gorilla/websocket"
+	zmq "github.com/pebbe/zmq4"
 )
 
 var addr = flag.String("addr", ":8080", "http service address")
 
-var upgrader = websocket.Upgrader{} // use default options
+var receiver *zmq.Socket
+var sender *zmq.Socket
+//var receq chan string
+//var sendq chan string
+
+var upgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+} 
+
+
+func reader(c *websocket.Conn){
+	for{
+		_, message, err := c.ReadMessage()
+		if err != nil {
+			log.Println("read:", err)
+			break
+		}
+		log.Printf("recv: %s", message)
+	}
+}
+func writer(c *websocket.Conn){
+	for{
+		s,err := receiver.Recv(0)
+		if(err!=nil){
+			log.Println("receiver.Recv err, ", err)
+		}
+		if(len(s)>0){
+			log.Println(s)
+			err := c.WriteMessage(websocket.TextMessage, []byte(s))
+			if err != nil {
+				log.Println("write:", err)
+				break
+			}
+		}
+		time.Sleep(time.Second)
+	}
+
+}
 
 func echo(w http.ResponseWriter, r *http.Request) {
 	c, err := upgrader.Upgrade(w, r, nil)
@@ -20,21 +60,9 @@ func echo(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	defer c.Close()
-	for {
-		mt, message, err := c.ReadMessage()
-		if err != nil {
-			log.Println("read:", err)
-			break
-		}
-		log.Printf("recv: %s", message)
-		err = c.WriteMessage(mt, message)
-		if err != nil {
-			log.Println("write:", err)
-			break
-		}
-	}
+	go writer(c)
+	reader(c)
 }
-
 func home(w http.ResponseWriter, r *http.Request) {
 	homeTemplate.Execute(w, "ws://"+r.Host+"/echo")
 }
@@ -42,7 +70,27 @@ func home(w http.ResponseWriter, r *http.Request) {
 func main() {
 	flag.Parse()
 	log.SetFlags(0)
+	var err error
+	sender,err = zmq.NewSocket(zmq.PUSH)
+	if(err!=nil){
+		log.Println("sender socket new err,", err)
+	}
+	defer sender.Close()
+	sender.Bind("tcp://*:5558")
+
+	receiver,err = zmq.NewSocket(zmq.PULL)
+	if(err!=nil){
+		log.Println("receiver socket new err,", err)
+	}
+	defer receiver.Close()
+
+	receiver.Bind("tcp://*:5557")
+
+	//receq = make(chan string)
+	//sendq = make(chan string)
+
 	http.HandleFunc("/echo", echo)
+
 	http.HandleFunc("/", home)
 	log.Fatal(http.ListenAndServe(*addr, nil))
 }
